0c01a487aad8542b079724472aec25c8188421c8
aishwarya <ashp17star@gmail.com>
2023-03-13 14:00:23 +0530

    Updated

--- 18_01_2023_GoogleMapsDemo/app/src/main/java/com/example/a18_01_2023_googlemapsdemo/MapsActivity.kt
+++ 18_01_2023_GoogleMapsDemo/app/src/main/java/com/example/a18_01_2023_googlemapsdemo/MapsActivity.kt
@@ -3,18 +3,20 @@ package com.example.a18_01_2023_googlemapsdemo
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Point
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.example.a18_01_2023_googlemapsdemo.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -22,6 +24,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private lateinit var bitcodeMarker : Marker
     private lateinit var shaniwarWadaMarker : Marker
+    private lateinit var circle : Circle
+    private lateinit var polygon : Polygon
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,12 +52,96 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         initMapSettings()
         addMarkers()
-
+        addShapes()
+        onInfoWindowClickListener()
+        onMarkerDragClickListener()
+        setInfoWindowAdapter()
+        miscellaneous()
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(bitcodeMarker.position))
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
+    private fun miscellaneous(){
+        mMap.setOnPoiClickListener {
+            Log.e("tag","${it.name} -- ${it.placeId} -- ${it.latLng}")
+        }
+    }
+    private fun addShapes(){
+        circle = mMap.addCircle(
+            CircleOptions().center(LatLng(18.4,73.80))
+                .radius(5000.0)
+                .strokeColor(Color.RED)
+                .strokeWidth(10.0F)
+                .fillColor(Color.MAGENTA)
+        )
+
+        polygon = mMap.addPolygon(
+            PolygonOptions()
+                .add(LatLng(23.21,72.63))
+                .add(LatLng(23.25,77.41))
+                .add(LatLng(21.25,81.62))
+                .add(LatLng(18.52,73.85))
+                .strokeWidth(10.0F)
+                .strokeColor(Color.CYAN)
+                .fillColor(Color.argb(5,255,0,0))
+        )
+    }
+    private fun onInfoWindowClickListener(){
+        mMap.setOnInfoWindowClickListener {
+            Log.e("tag","${it.title}")
+            Toast.makeText(this,"${it.title} -- ${it.position.longitude} -- ${it.position.latitude}",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun onMarkerDragClickListener(){
+        mMap.setOnMarkerDragListener(
+            object : GoogleMap.OnMarkerDragListener{
+                override fun onMarkerDragStart(marker: Marker) {
+                   Log.e("onStart","${marker.title} -- ${marker.position.latitude} -- ${marker.position.longitude}")
+                }
+
+                override fun onMarkerDrag(marker: Marker) {
+                    Log.e("drag","${marker.title} -- ${marker.position.latitude} -- ${marker.position.longitude}")
+                }
+
+                override fun onMarkerDragEnd(marker: Marker) {
+                    Log.e("onEnd","${marker.title} -- ${marker.position.latitude} -- ${marker.position.longitude}")
+                }
+            }
+        )
+    }
+
+   /* private fun markerClickListener(){
+        mMap.setOnMarkerClickListener {
+           object : GoogleMap.OnMarkerClickListener{
+               override fun onMarkerClick(p0: Marker): Boolean {
+                   Log.e("tag","${p0.title}")
+                    return true
+               }
+           }
+        }
+    }*/
+
+    private fun setInfoWindowAdapter(){
+        mMap.setInfoWindowAdapter(MyInfoWindowAdapter())
+    }
+    private inner class MyInfoWindowAdapter : GoogleMap.InfoWindowAdapter{
+        override fun getInfoContents(p0: Marker): View? {
+            var infoWindowView = layoutInflater.inflate(R.layout.my_info_window,null)
+            var infoWindowBinding = com.example.a18_01_2023_googlemapsdemo.databinding.MyInfoWindowBinding.bind(infoWindowView)
+            infoWindowBinding.txtName.text = p0.title
+            infoWindowBinding.txtPosition.text = p0.position.toString()
+            return infoWindowView
+        }
+
+        override fun getInfoWindow(p0: Marker): View? {
+           return null
+        }
+    }
+
+
     private fun addMarkers(){
         var markerOptionsForBitcodeMarker = MarkerOptions().title("Bitcode")
             .snippet("Bitcode Office")
@@ -66,6 +154,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .alpha(0.8F)
             .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
         bitcodeMarker = mMap.addMarker(markerOptionsForBitcodeMarker)!!
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(bitcodeMarker.position))
+        mMap.moveCamera(CameraUpdateFactory.zoomBy(2000.0F))
+
+
 
         var scaledImage = Bitmap.createScaledBitmap(
             BitmapFactory.decodeResource(resources, R.drawable.shaniwarwada),
